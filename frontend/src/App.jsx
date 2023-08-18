1b6107bd716bf4e4f48a47c32d23bf7c790f9749
nguyenpatz <nguyenpatzz@gmail.com>
2023-08-19 01:05:26 +0700

    add file upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import "./styles/app.css";
 import Chatbot from "./components/chatbot";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Main />
       <Chatbot />
     </div>
   );
---
+++ frontend/src/components/tab/FireTune.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function FineTune() {
+    return (
+        <div className="container tab">
+            FineTune
+            <div className="container__top"></div>
+            <div className="container__middle"></div>
+            <div className="container__bottom"></div>
+        </div>
+    );
+}
